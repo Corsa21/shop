e42ca39723a358e230b819b77e7def3aca6cf3e0
Corsa21 <kirill.assassin@yandex.ru>
2022-02-14 17:41:48 +0700

    edit basket

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useContext } from 'react';
+import React, { useState, createContext, useContext, useEffect, useCallback } from 'react';
 import { ReactComponent as BasketIcon } from '../../icons/basket.svg';
 import './Basket.scss';
 
@@ -7,24 +7,65 @@ export const BasketContext = createContext();
 
 export default function Basket(props) {
 
-    const [open, setOpen] = useState(false);
-    const {basket} = useContext(BasketContext)
+  const [open, setOpen] = useState(false);
+  const { basket } = useContext(BasketContext)
+
+  useEffect(()=>{
+    console.log(basket)
+  }, [basket])
+
+  const calculateTotalPrice = useCallback(() => {
+    const basketPrices = basket.map((elem)=>{
+      return +elem.count * +elem.cost
+    })
+    let result = 0;
+
+    basketPrices.forEach((elem)=>{
+      result += elem
+    })
+
+    return result
+  }, [basket])
 
   return (
     <div className="basket">
-        <div className="basket__button" onClick={()=>setOpen(!open)}>
-            <BasketIcon/>
-        </div>
-        { open && <div className="basket__modal">
-          {basket.map((el, i) => <div key={i} className="basket__item">
-            <div className="basket__product">
-             Name: <br/><span className='basket__name'>{el.title}</span> <br/>Cost: <br/><span className='basket__cost'>{el.cost}</span>
-            </div>
-          </div>)}
-        </div>
-        }
-
-
-    </div>
+      <table className='basket__modal' style={{display:open ? 'block' : 'none'}}>
+        
+        <thead>
+          <tr>
+            <td>Название товара</td>
+            <td>Количество</td>
+            <td>Цена за штуку</td>
+            <td>Итого</td>
+          </tr>
+        </thead>
+        <tbody>
+            {basket.map((elem)=>(
+              <tr>
+                <td>{elem.title}</td>
+                <td>{elem.count}</td>
+                <td>$ {elem.cost}</td>
+                <td>$ {+elem.count * +elem.cost}</td>
+              </tr>
+            ))}
+              <tr>
+                <td></td>
+                <td></td>
+                <td>Итог:</td>
+                <td>$ {calculateTotalPrice()}</td>
+              </tr>
+        </tbody>
+      </table>
+       <div className="basket__button" onClick={() => setOpen(!open)}>
+        <BasketIcon />
+      </div>
+      {/* {open && <div className="basket__modal">
+        {basket.map((el, i) => <div key={i} className="basket__item">
+          <div className="basket__product">
+            Name: <br /><span className='basket__name'>{el.title}</span> <br />Cost: <br /><span className='basket__cost'>{el.cost}</span>
+          </div>
+        </div>)}  */}
+      </div>
+    
   );
 }
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,5 +1,5 @@
 import './ProductCard.scss';
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import { BasketContext } from '../Basket/Basket';
 import { useContext } from 'react';
 
@@ -7,7 +7,30 @@ import { useContext } from 'react';
 
 function ProductCard(props) {
 
-    const {setBasket} = useContext(BasketContext);
+    const { setBasket } = useContext(BasketContext);
+
+    const addToBasket = (event) => {
+        event.preventDefault();
+        setBasket((prev) => {
+            let duplicateIndex;
+            const duplicate = prev.find((elem, i) => {
+                if (elem.title === props.title) {
+                    duplicateIndex = i;
+                    return true;
+                }
+                return false;
+            })
+
+            if (duplicate) {
+                const newBasket = [...prev];
+                newBasket[duplicateIndex].count += 1;
+                return newBasket;
+            } else {
+                const basketItem = { ...props, count: 1 };
+                return prev.concat([basketItem]);
+            }
+        })
+    }
 
     return (
         <Link to="/product" className="product-card">
@@ -16,14 +39,11 @@ function ProductCard(props) {
             <div className="product-card__text">{props.text}</div>
             <div className="product-card__cost">$ {props.cost}</div>
             <div className="product-card__layer">
-                <button 
-                    onClick={(event)=> {
-                        event.preventDefault();
-                        setBasket( (prev)=> prev.concat([props]))
-                    }}
+                <button
+                    onClick={addToBasket}
                     className="product-card__btn"
                 >
-                <svg id="Outlined" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><title/><g id="Fill"><path d="M23.78,11,22.35,5.27A3,3,0,0,0,19.44,3H12.56A3,3,0,0,0,9.65,5.27L8.22,11H2.16L6.09,26.73A3,3,0,0,0,9,29H23a3,3,0,0,0,2.91-2.27L29.84,11ZM15,21v6H12.83L11.7,21Zm-3.67-2L10.2,13H15v6ZM17,21h3.3l-1.13,6H17Zm0-2V13h4.8l-1.13,6Zm6.83-6h3.45l-1.5,6H22.7ZM11.59,5.76a1,1,0,0,1,1-.76h6.88a1,1,0,0,1,1,.76L21.72,11H10.28ZM8.17,13,9.3,19H6.22l-1.5-6ZM8,26.24,6.72,21h3l1.12,6H9A1,1,0,0,1,8,26.24Zm15.94,0A1,1,0,0,1,23,27H21.21l1.12-6h3Z"/></g></svg>
+                    <svg id="Outlined" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><title /><g id="Fill"><path d="M23.78,11,22.35,5.27A3,3,0,0,0,19.44,3H12.56A3,3,0,0,0,9.65,5.27L8.22,11H2.16L6.09,26.73A3,3,0,0,0,9,29H23a3,3,0,0,0,2.91-2.27L29.84,11ZM15,21v6H12.83L11.7,21Zm-3.67-2L10.2,13H15v6ZM17,21h3.3l-1.13,6H17Zm0-2V13h4.8l-1.13,6Zm6.83-6h3.45l-1.5,6H22.7ZM11.59,5.76a1,1,0,0,1,1-.76h6.88a1,1,0,0,1,1,.76L21.72,11H10.28ZM8.17,13,9.3,19H6.22l-1.5-6ZM8,26.24,6.72,21h3l1.12,6H9A1,1,0,0,1,8,26.24Zm15.94,0A1,1,0,0,1,23,27H21.21l1.12-6h3Z" /></g></svg>
                     Add to cart
                 </button>
             </div>
